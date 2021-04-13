384165f7bafaa4cc6302e0c27248e1a12c9a49d7
guangliangliang <47070735+guangliangliang@users.noreply.github.com>
2021-04-13 23:10:54 +0800

    feat add base Table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import "./index.less";
 import AntdForm from "./AntdForm";
+import TestTable from "./TestTable";
 import "antd/dist/antd.css";
 import { tableFormData } from "./Data/tempData";
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className="App">
       <h1>数据生成表单</h1>
       <AntdForm formData={tableFormData} />
+      <TestTable />
     </div>
   );
 }
